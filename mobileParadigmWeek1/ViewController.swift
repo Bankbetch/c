04ab53630d2491aa3f38d57fed7cf4df6bfe13ff
Jiraphun Jitrukrob <lharm16532@gmail.com>
2019-01-09 14:19:43 +0700

    Initial Commit

---
+++ mobileParadigmWeek1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mobileParadigmWeek1
+//
+//  Created by Bank on 9/1/2562 BE.
+//  Copyright © 2562 Bank. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
